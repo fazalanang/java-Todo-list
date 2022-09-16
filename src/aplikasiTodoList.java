90a94846c8c9a2db6b7866a71678329269f32b4b
Faza Lanang <faza.lanangdjati@gmail.com>
2022-09-16 11:37:41 +0700

    add todo ke list

--- src/aplikasiTodoList.java
+++ src/aplikasiTodoList.java
@@ -29,8 +29,34 @@ public class aplikasiTodoList {
     /**
      * Menambahkan todo ke list
      */
-    public static void addTodoList() {
+    public static void addTodoList(String todo) {
+        //cek apakah model penuh?
+        var isFull = true;
+        for (int i = 0; i < model.length; i++) {
+            if (model[i] == null) {
+                //model masih ada yg kosong
+                isFull = false;
+                break;
+            }
+        }
 
+        //jika penuh, kita resize ukuran array nya 2x lipat
+        if (isFull) {
+            var temp = model;
+            model = new String [model.length*2];
+
+            for (int i = 0; i < temp.length; i++) {
+                model[i] = temp[i];
+            }
+        }
+
+        //tambahkan ke posisi yang data array nya NULL
+        for (int i = 0; i < model.length; i++) {
+            if (model[i] == null) {
+                model[i] = todo;
+                break;
+            }
+        }
     }
 
     /**
