01089c70fd63418d257e6d7fab230373d7e5bfb0
Faza Lanang <faza.lanangdjati@gmail.com>
2022-09-16 10:48:46 +0700

    first commit

---
+++ src/aplikasiTodoList.java
@@ -0,0 +1,28 @@
+public class aplikasiTodoList {
+    public static String[] model = new String[10];
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Menampilkan todo list
+     */
+    public static void showTodoList() {
+
+    }
+
+    /**
+     * Menambahkan todo ke list
+     */
+    public static void addTodoList() {
+
+    }
+
+    /**
+     * Menghapus todo dari list
+     */
+    public static void removeTodoList() {
+
+    }
+}
