9491d2b831f9d28da16daa820e190b983a1ba83c
Faza Lanang <faza.lanangdjati@gmail.com>
2022-09-16 11:02:45 +0700

    show todo list

--- src/aplikasiTodoList.java
+++ src/aplikasiTodoList.java
@@ -9,6 +9,14 @@ public class aplikasiTodoList {
      * Menampilkan todo list
      */
     public static void showTodoList() {
+        for (var i = 0; i < model.length; i++) {
+            var todo = model[i];
+            var no = i + 1;
+
+            if (todo != null) {
+                System.out.println(no + "." + todo);
+            }
+        }
 
     }
 
